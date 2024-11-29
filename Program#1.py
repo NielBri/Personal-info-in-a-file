1d173d2d26ad670da5fd52fb3a49fa3b1b60df9f
NielBri <briones.nielmatthew@gmail.com>
2024-11-29 14:55:37 +0800

    Pseudocode for Program 1

---
+++ Program#1.py
@@ -0,0 +1,7 @@
+# Program 1 is about inputting 5 info about a person and put it in a file
+# Make list
+# Add inputs and indicate when to stop
+# Store inputs in a Library and put it in a variable
+# Append variable in a list
+# Write the list in a file (specifically the Information.txt file)
+
