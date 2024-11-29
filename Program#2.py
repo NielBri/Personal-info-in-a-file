daa609a3d9bc7efc6068a25242e4f90d0bff9648
NielBri <briones.nielmatthew@gmail.com>
2024-11-29 15:26:45 +0800

    Pseudo code for Program 2

---
+++ Program#2.py
@@ -0,0 +1,4 @@
+# Program 2 ties to get the info on the text file and searches for a match
+# Search for user in the file
+# Read the lines in the file
+# Search in the file for a file with the same name use also an if function
