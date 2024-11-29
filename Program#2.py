09b6fcdb8d414676641e884bc89d3114ec9a6647
NielBri <briones.nielmatthew@gmail.com>
2024-11-29 16:00:58 +0800

    Added the detect for the same name

--- Program#2.py
+++ Program#2.py
@@ -6,5 +6,17 @@ ask_user = input("Search for user: ").strip()
 # Read the lines in the file
 with open('Writing.txt', 'r') as file_handle:
     lines = file_handle.readlines()
-    
-# Search in the file for a file with the same name use also an if function
+
+    # Search in the file for a file with the same name use also an if function
+    for line in lines:
+        if info['Name'] == ask_user:
+            print("Person found!\n")
+            print("Name: ", info["Name"])
+            print("Age: ", info["Age"])
+            print("Salary: ", info["Salary"])
+            print("Address: ", info["Address"])
+            print("Favorite food: ", info["Favorite Food"])
+
+
+        else:
+            print("User not found")
