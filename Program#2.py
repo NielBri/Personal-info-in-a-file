9beb434bce1143bdd8fd31100da9419bf234ec4d
NielBri <briones.nielmatthew@gmail.com>
2024-11-29 15:35:12 +0800

    Added the input and read functions

--- Program#2.py
+++ Program#2.py
@@ -1,4 +1,10 @@
 # Program 2 ties to get the info on the text file and searches for a match
+
 # Search for user in the file
+ask_user = input("Search for user: ").strip()
+
 # Read the lines in the file
+with open('Writing.txt', 'r') as file_handle:
+    lines = file_handle.readlines()
+    
 # Search in the file for a file with the same name use also an if function
